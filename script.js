a59b434f10f397d20adc51b6795dd10a8103a691
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-10-08 07:18:13 -0700

    created a submission

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const makeChange = (c) => {
 		n: 0,
 		p: 0
 	};
-	change.q = Math.floor(c/25);
+	change.q = Math.floor(c / 25);
 	c = c % 25;
 
-	change.d = Math.floor(c/10);
-	c = c % 25;
+	change.d = Math.floor(c / 10);
+	c = c % 10;
 
 	change.n = Math.floor(c / 5);
     c = c % 5;
